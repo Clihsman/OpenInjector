976ec189ad6961f455877aed1577540bdd99264f
Clihsman <53985190+Clihsman@users.noreply.github.com>
2025-01-03 17:58:54 -0500

    cambios en proceso

--- OpenInjector/Attributes/BuilderAttribute.cs
+++ OpenInjector/Attributes/BuilderAttribute.cs
@@ -3,4 +3,11 @@
 [AttributeUsage(AttributeTargets.Method)]
 public class BuilderAttribute : Attribute
 {
+    public Attribute? RequiredAttribute { get; private set; }
+
+    public BuilderAttribute() { }
+
+    public BuilderAttribute(Attribute? requiredAttribute) {
+        RequiredAttribute = requiredAttribute;
+    }
 }
